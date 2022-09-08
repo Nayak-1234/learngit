bf1cf806106b9e2e9ffa5f58646b9bed01dab9c6
Raju <raju@gmail.com>
2022-09-08 07:08:26 +0000

    Difference classs is added

--- src/Raju.java
+++ src/Raju.java
@@ -1,5 +1,6 @@
 public class Raju {
     public static void main(String[] args) {
         System.out.println("hello git welcome to git world Iam Raju");
+        Sum.add(2,3);
     }
 }
---
+++ src/Sum.java
@@ -0,0 +1,5 @@
+public class Sum {
+   static void add(int a, int b) {
+        System.out.println(a+b);
+    }
+}
